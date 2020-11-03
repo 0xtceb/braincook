ece9074f8495e72e13dd6f618a8bb4511d26db09
vagrant <vagrant@vagrant.vm>
2020-11-03 21:20:04 +0000

    Fix: Added auth guard to providers list

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,8 +1,9 @@
 import { SigninComponent, SignupComponent } from './auth';
 import { AuthService } from './services';
+import { AuthGuard } from './shared/guard/auth.guard';
 
 export * from './auth/index';
 export * from './services/index';
 
 export const components = [SigninComponent, SignupComponent];
-export const providers = [AuthService];
+export const providers = [AuthService, AuthGuard];
