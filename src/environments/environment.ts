e7807bbb60b93dd85816e4a877a8ac479fe1d1b6
vagrant <vagrant@vagrant.vm>
2020-11-04 21:21:45 +0000

    fix: env

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBX9s029a6V6s21MtLr9XBc-Aiudr8RwuU',
+    authDomain: 'braincook-35256.firebaseapp.com',
+    databaseURL: 'https://braincook-35256.firebaseio.com/',
+    projectId: 'braincook-35256',
+    storageBucket: 'gs://braincook-35256.appspot.com',
+    messagingSenderId: 'xxxxxx',
+    appId: '301190514841'
+  }
+};
